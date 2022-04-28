f7d73efa9091ad064c7f4f5c3c5a7fa36b0c9ca8
Andre Neves <andre@zebedee.io>
2022-04-27 20:44:24 -0400

    chore: linting

--- src/utils/colors.js
+++ src/utils/colors.js
@@ -1,5 +1,5 @@
-import { TAG_COLORS, DEFAULT_OVERLAY_COLOR } from "../constants/tag-colors";
+import { TAG_COLORS, DEFAULT_OVERLAY_COLOR } from '../constants/tag-colors'
 
 export const getTagColor = (name) => {
-  return TAG_COLORS[name] || DEFAULT_OVERLAY_COLOR;
+  return TAG_COLORS[name] || DEFAULT_OVERLAY_COLOR
 }
